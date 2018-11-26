4e109409e30ef609bca6ec37c688d2a213424fc7
heislercreative <brennan@heislercreative.com>
2018-11-25 17:15:10 -0700

    add User seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+User.create(email: 'brennan@heislercreative.com', password: 'testpw', business_name: 'Heisler Creative Services', first_name: 'Brennan', last_name: 'Heisler', address: '1234 Wamapoke Way', city: 'Pawnee', state_initials: 'IN', zip: 47424, phone: 5555545555)
