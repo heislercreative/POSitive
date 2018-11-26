8d3bf2fd1f89e2e3b7c2c11408ad5228df62b171
heislercreative <brennan@heislercreative.com>
2018-11-25 17:23:51 -0700

    set User index & show API points

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,11 @@
 class UsersController < ApiController
   def index
-    @users = User.all
+    @users = User.select('id', 'email', 'business_name', 'first_name', 'last_name').all
     render json: @users
+  end
+
+  def show
+    @user = User.find(params[:id])
+    render json: @user
   end
 end
