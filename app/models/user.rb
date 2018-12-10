9c6233ae43c8f9bedad00268e264b59ba82bee23
heislercreative <brennan@heislercreative.com>
2018-12-09 21:32:11 -0700

    start Yelp scraper method

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sites
+
+
+
+
+
+  #Scraper Methods
+    def self.scrape_yelp
+      yelp_site = self.sites.where("platform = 'Yelp'")
+      yelp_url = yelp_site.profile_url
+      yelp_page = Nokogiri::html(open("#{yelp_url}"))
+    end
 end
