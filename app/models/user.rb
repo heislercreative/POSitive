fde33dab660b691854befa5d0a68d74e82d369c9
heislercreative <brennan@heislercreative.com>
2018-12-12 06:47:26 -0700

    start Yelp scraper & update Ruby version

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,20 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sites
+  require 'open-uri'
 
 
 
 
 
   #Scraper Methods
-    def self.scrape_yelp
-      yelp_site = self.sites.where("platform = 'Yelp'")
-      yelp_url = yelp_site.profile_url
-      yelp_page = Nokogiri::html(open("#{yelp_url}"))
+    def scrape_yelp
+      yelp_url = self.sites.where(platform: 'Yelp').first.profile_url
+      yelp_page = Nokogiri::html(open(yelp_url))
+      yelp_title = yelp_page.css("h1.biz-page-title")
+      # yelp_rating_div = yelp_page.css("div.biz-rating.biz-rating-very-large.clearfix")
+      # yelp_rating = yelp_rating_div.css("div.i-stars").title
+      # yelp_rating
+      yelp_title
     end
 end
