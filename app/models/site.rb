b435c7282d595078df8d797d51c715bb138fe35f
heislercreative <brennan@heislercreative.com>
2019-01-13 15:04:21 -0700

    get Yelp review_count via API

--- app/models/site.rb
+++ app/models/site.rb
@@ -10,15 +10,17 @@ class Site < ApplicationRecord
     end
   end
 
-  def yelp_rating
+  def yelp_info
     slugify
     @resp = Faraday.get "https://api.yelp.com/v3/businesses/#{self.slug}" do |req|
       req.headers['Authorization'] = ENV['YELP_API_KEY']
     end
     resp_body = JSON.parse(@resp.body)
     rating = resp_body["rating"]
+    review_count = resp_body["review_count"]
     puts rating
-    self.update(rating: rating)
+    puts review_count
+    self.update(rating: rating, review_count: review_count)
   end
 
 end
